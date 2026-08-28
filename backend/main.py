d8883e1178de1040e7d1bbbb5ec1a38d84b93890
Maria <mariawt.mm@gmail.com>
2026-08-28 20:33:23 -0300

    config: Adicionando link para o deploy no vercel

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "https://kivira.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
